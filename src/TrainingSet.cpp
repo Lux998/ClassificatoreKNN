ce74de53b7e487dfe80820c70812dff6fac6ac87
basi24 <basili24@hotmail.it>
2019-06-25 16:47:04 +0200

    Aggiunti casi test TrainingSet

--- src/TrainingSet.cpp
+++ src/TrainingSet.cpp
@@ -7,6 +7,15 @@ std::vector<Documento*> vec;
 TrainingSet::TrainingSet()
 {
     this->aggiungiDoc("ArticoloPolitica1.txt","politica");
+    this->aggiungiDoc("ArticoloPolitica2.txt","politica");
+    this->aggiungiDoc("ArticoloPolitica3.txt","politica");
+    this->aggiungiDoc("ArticoloSport1.txt","sport");
+    this->aggiungiDoc("ArticoloSport2.txt","sport");
+    this->aggiungiDoc("ArticoloSport3.txt","sport");
+    this->aggiungiDoc("ArticoloFinanza1.txt","finanza");
+    this->aggiungiDoc("ArticoloFinanza2.txt","finanza");
+    this->aggiungiDoc("ArticoloFinanza3.txt","finanza");
+
 }
 
 TrainingSet::~TrainingSet()
